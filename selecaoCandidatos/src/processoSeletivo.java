0c13bd5cfcf97f0f0f3fc7850e3a55096583dd6c
hiago fernando <hiago30fernando@hotmail.com>
2023-08-28 22:19:23 -0300

    Dia 28/08

--- selecaoCandidatos/src/processoSeletivo.java
+++ selecaoCandidatos/src/processoSeletivo.java
@@ -1,5 +1,90 @@
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class processoSeletivo {
     public static void main(String[] args) throws Exception {
-        
+        String [] candidatosSelecionados = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+		//primeiro um for para selecionar os candidatos
+		for(String candidato: candidatosSelecionados) {
+			entrandoEmContato(candidato);
+		}
     }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+        do {
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if(continuarTentando)
+				tentativasRealizadas++;
+			else System.out.println("CONTATO REALIZADO COM SUCESSO");
+
+        }while(continuarTentando && tentativasRealizadas<3);
+        
+        if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+
+    }
+
+    
+    //método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
+    
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        
+        System.out.println("Imprimindo a lista de candidatos informando o indece do elemento!");
+        
+        for(int indice=0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de numero " + (indice+1) + " é " + candidatos[indice]);
+        }
+
+        //forma abrevida
+		//interação total sem precisar da posição ou indice
+		System.out.println("Imprimindo todos sem a necessidade de exibir o índice");
+		
+		for(String candidato: candidatos) {
+			System.out.println("O candidato selecionado foi " + candidato);
+		}
+
+    }
+
+    static void selecaoCandidatos(){
+        // Array com a lista de candidatos
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual=0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salario " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+        System.out.println("Ligar para o Candidato");
+        } else if (salarioBase==salarioPretendido)
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+}
 }
